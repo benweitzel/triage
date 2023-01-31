103653ced8a6f889ddd1b9ab2c7a5953e6fa9014
benweitzel <benweitzel@users.noreply.github.com>
2023-01-31 10:16:53 -0500

    Question explanations (except SALT)

--- start.js
+++ start.js
@@ -9,6 +9,12 @@ function walk_yes() {
     document.getElementById('resp').style.display = "none";
     document.getElementById('perfusion').style.display = "none";
     document.getElementById('mental').style.display = "none";
+    document.getElementById('q1explain').style.display = "block"
+    document.getElementById('q2explain').style.display = "none"
+    document.getElementById('q3-1explain').style.display = "none"
+    document.getElementById('q3-2explain').style.display = "none"
+    document.getElementById('q4explain').style.display = "none"
+    document.getElementById('q5explain').style.display = "none"
 }
 
    function walk_no() {
@@ -18,6 +24,12 @@ function walk_yes() {
    document.getElementById("immediate").style.display = "none";
    document.getElementById('expectant').style.display = "none";
    document.getElementById('breath1').style.display = "block";
+   document.getElementById('q1explain').style.display = "none"
+   document.getElementById('q2explain').style.display = "block"
+   document.getElementById('q3-1explain').style.display = "none"
+   document.getElementById('q3-2explain').style.display = "none"
+   document.getElementById('q4explain').style.display = "none"
+   document.getElementById('q5explain').style.display = "none"
 }
 
    function breath1_yes() {
@@ -30,6 +42,12 @@ function walk_yes() {
    document.getElementById('resp').style.display = "block";
    document.getElementById('perfusion').style.display = "none";
    document.getElementById('mental').style.display = "none";
+   document.getElementById('q1explain').style.display = "none"
+   document.getElementById('q2explain').style.display = "none"
+   document.getElementById('q3-1explain').style.display = "none"
+   document.getElementById('q3-2explain').style.display = "block"
+   document.getElementById('q4explain').style.display = "none"
+   document.getElementById('q5explain').style.display = "none"
 }
    function breath1_no() {
    document.getElementById('placeholder').style.display = "block";
@@ -41,6 +59,12 @@ function walk_yes() {
    document.getElementById('resp').style.display = "none";
    document.getElementById('perfusion').style.display = "none";
    document.getElementById('mental').style.display = "none";
+   document.getElementById('q1explain').style.display = "none"
+   document.getElementById('q2explain').style.display = "none"
+   document.getElementById('q3-1explain').style.display = "block"
+   document.getElementById('q3-2explain').style.display = "none"
+   document.getElementById('q4explain').style.display = "none"
+   document.getElementById('q5explain').style.display = "none"
 }
 
    function breath2_yes() {
@@ -50,6 +74,12 @@ function walk_yes() {
    document.getElementById("immediate").style.display = "block";
    document.getElementById('expectant').style.display = "none";
    document.getElementById('expired').style.display = "block";
+   document.getElementById('q1explain').style.display = "none"
+   document.getElementById('q2explain').style.display = "none"
+   document.getElementById('q3-1explain').style.display = "block"
+   document.getElementById('q3-2explain').style.display = "none"
+   document.getElementById('q4explain').style.display = "none"
+   document.getElementById('q5explain').style.display = "none"
 }
     function breath2_no() {
    document.getElementById('placeholder').style.display = "none";
@@ -58,6 +88,12 @@ function walk_yes() {
    document.getElementById("immediate").style.display = "none";
    document.getElementById('expectant').style.display = "block";
    document.getElementById('expired').style.display = "none";
+   document.getElementById('q1explain').style.display = "none"
+   document.getElementById('q2explain').style.display = "none"
+   document.getElementById('q3-1explain').style.display = "block"
+   document.getElementById('q3-2explain').style.display = "none"
+   document.getElementById('q4explain').style.display = "none"
+   document.getElementById('q5explain').style.display = "none"
 }
 
     function resp_lt30() {
@@ -68,6 +104,13 @@ function walk_yes() {
     document.getElementById('expectant').style.display = "none";
     document.getElementById('perfusion').style.display = "block";
     document.getElementById('mental').style.display = "none";
+    document.getElementById('q1explain').style.display = "none"
+    document.getElementById('q2explain').style.display = "none"
+    document.getElementById('q3-1explain').style.display = "none"
+    document.getElementById('q3-2explain').style.display = "none"
+    document.getElementById('q4explain').style.display = "block"
+    document.getElementById('q5explain').style.display = "none"
+
 }
     function resp_gt30() {
     document.getElementById('placeholder').style.display = "none";
@@ -76,6 +119,12 @@ function walk_yes() {
     document.getElementById('immediate').style.display = "block";
     document.getElementById('expectant').style.display = "none";
     document.getElementById('perfusion').style.display = "none";
+    document.getElementById('q1explain').style.display = "none"
+    document.getElementById('q2explain').style.display = "none"
+    document.getElementById('q3-1explain').style.display = "none"
+    document.getElementById('q3-2explain').style.display = "block"
+    document.getElementById('q4explain').style.display = "none"
+    document.getElementById('q5explain').style.display = "none"
 }
 
     function perfusion_yes() {
@@ -85,6 +134,12 @@ function walk_yes() {
     document.getElementById("immediate").style.display = "none";
     document.getElementById('expectant').style.display = "none";
     document.getElementById('mental').style.display = "block";
+    document.getElementById('q1explain').style.display = "none"
+    document.getElementById('q2explain').style.display = "none"
+    document.getElementById('q3-1explain').style.display = "none"
+    document.getElementById('q3-2explain').style.display = "none"
+    document.getElementById('q4explain').style.display = "none"
+    document.getElementById('q5explain').style.display = "block"
 }
     function perfusion_no() {
     document.getElementById('placeholder').style.display = "none";
@@ -93,6 +148,12 @@ function walk_yes() {
     document.getElementById("immediate").style.display = "block";
     document.getElementById('expectant').style.display = "none";
     document.getElementById('mental').style.display = "none";
+    document.getElementById('q1explain').style.display = "none"
+    document.getElementById('q2explain').style.display = "none"
+    document.getElementById('q3-1explain').style.display = "none"
+    document.getElementById('q3-2explain').style.display = "none"
+    document.getElementById('q4explain').style.display = "block"
+    document.getElementById('q5explain').style.display = "none"
 }
 
     function mental_yes() {
@@ -101,6 +162,12 @@ function walk_yes() {
     document.getElementById("delayed").style.display = "block";
     document.getElementById("immediate").style.display = "none";
     document.getElementById('expectant').style.display = "none";
+    document.getElementById('q1explain').style.display = "none"
+    document.getElementById('q2explain').style.display = "none"
+    document.getElementById('q3-1explain').style.display = "none"
+    document.getElementById('q3-2explain').style.display = "none"
+    document.getElementById('q4explain').style.display = "none"
+    document.getElementById('q5explain').style.display = "block"
 }
     function mental_no() {
     document.getElementById('placeholder').style.display = "none";
@@ -108,6 +175,12 @@ function walk_yes() {
     document.getElementById("delayed").style.display = "none";
     document.getElementById("immediate").style.display = "block";
     document.getElementById('expectant').style.display = "none";
+    document.getElementById('q1explain').style.display = "none"
+    document.getElementById('q2explain').style.display = "none"
+    document.getElementById('q3-1explain').style.display = "none"
+    document.getElementById('q3-2explain').style.display = "none"
+    document.getElementById('q4explain').style.display = "none"
+    document.getElementById('q5explain').style.display = "block"
 }
 
 function refresh() {
